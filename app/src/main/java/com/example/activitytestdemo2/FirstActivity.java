69e870192c24cab993e0369504c0d6641d047618
foreverjava <2993515925@qq.com>
2022-12-14 22:08:34 +0800

    my first app

---
+++ app/src/main/java/com/example/activitytestdemo2/FirstActivity.java
@@ -0,0 +1,27 @@
+package com.example.activitytestdemo2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);//这是我们加的代码
+        Button button = findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Toast.makeText(FirstActivity.this,
+//                        "這是我的第一個android程序  ", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
